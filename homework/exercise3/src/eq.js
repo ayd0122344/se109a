70151d2d2c51dec45bf16ce5ac9b04d817150898
ayd0122344 <ycvs210@gmail.com>
2021-01-08 10:10:38 +0800

    eq.js complete

--- homework/exercise3/src/eq.js
+++ homework/exercise3/src/eq.js
@@ -1,4 +1,5 @@
-export function eq(a,b) {
-    if(a===b){return true}
-    else return false
+export function eq(a, b) {
+    if (Number.isNaN(a) && Number.isNaN(b)){return true}
+    else if(a !== b){return false}
+    else return true
   }
